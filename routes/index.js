0a953390d27250a3b0bf614d0468181d9afb4687
zhangzhaopds <zhangzhaopds@foxmail.com>
2017-07-07 09:18:22 +0800

    222

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var log4js = require('../app');
+// var log4js = require('../app');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var logger = log4js.logger(__dirname + '/index');
-    logger.debug('你比');
-    logger.info("This i    kkkkkkkk   s an index page!");
+    // var logger = log4js.logger(__dirname + '/index');
+    // logger.debug('你比');
+    // logger.info("This i    kkkkkkkk   s an index page!");
     // logger.error("错误错误粗我");
     // logger.error("错误");
     // logger.warn("警告");
