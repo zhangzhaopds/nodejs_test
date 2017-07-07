60e2e09dcd05c705a37b1bb3b4f45eb467a1cb9c
zhangzhaopds <zhangzhaopds@foxmail.com>
2017-07-07 09:43:54 +0800

    ddd

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-var log4js = require('../app');
+// var log4js = require('../app');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var logger = log4js.logger(__dirname + '/index');
-    logger.debug('你比');
-    logger.info("This i    kkkkkkkk   s an index page!");
-    logger.error("错误错误粗我");
-    logger.error("错误");
-    logger.warn("警告");
+    // var logger = log4js.logger(__dirname + '/index');
+    // logger.debug('你比');
+    // logger.info("This i    kkkkkkkk   s an index page!");
+    // logger.error("错误错误粗我");
+    // logger.error("错误");
+    // logger.warn("警告");
     res.render('index', { title: 'Express' });
 });
 
